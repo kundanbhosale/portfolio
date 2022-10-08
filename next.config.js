ba24cfb0b11755934b65c108c3706f2710f91725
Kundan Bhosale <kundanbhosale99@gmail.com>
2022-10-08 13:48:10 +0530

    added loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === 'true',
 })
 module.exports = withBundleAnalyzer({
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 })
